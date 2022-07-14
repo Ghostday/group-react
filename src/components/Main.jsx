7bf01e00cce18bfa4d0dd1c0afa5e8faef18e14c
Chris <fablekey@gmail.com>
2022-07-14 10:35:02 +1000

    new main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function MainBody() {
+  return (
+    <div id="main-content">
+      <h1>Platypod is the best pod!</h1>
+      <p>Sorry Gryffindor and Code Club, you guys just aren't that great.</p>
+    </div>
+  )
+}
